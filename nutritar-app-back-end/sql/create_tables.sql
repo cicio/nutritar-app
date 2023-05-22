b05c008a0c9aba0afd0c7213e3af344bb4697ce7
mwilsoncruz <8440577+cicio@users.noreply.github.com>
2023-05-21 22:46:40 -0400

    fixed create_tables.sql script failure to insert records

--- nutritar-app-back-end/sql/create_tables.sql
+++ nutritar-app-back-end/sql/create_tables.sql
@@ -21,23 +21,23 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: gombs; Type: TABLE; Schema: public; Owner: -
+-- Name: gsbombs; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.gombs (
+CREATE TABLE public.gsbombs (
     id integer NOT NULL,
-    gombs_title character varying(255),
+    gsbomb character varying(255),
     created_at timestamp without time zone,
     updated_at timestamp without time zone
 );
 
 
 --
--- Name: gombs_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+-- Name: gsbombs_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-ALTER TABLE public.gombs ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.gombs_id_seq
+ALTER TABLE public.gsbombs ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.gsbombs_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -52,8 +52,8 @@ ALTER TABLE public.gombs ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 CREATE TABLE public.foods (
     id integer NOT NULL,
-    fdb_id character varying(32),
-    food_title character varying(128),
+    data_bank_id character varying(512),
+    food_name character varying(512),
     food_description text,
     food_image character varying(255),
     created_at timestamp without time zone,
@@ -62,22 +62,22 @@ CREATE TABLE public.foods (
 
 
 --
--- Name: foods_gombs; Type: TABLE; Schema: public; Owner: -
+-- Name: foods_gsbombs; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.foods_gombs (
+CREATE TABLE public.foods_gsbombs (
     id integer NOT NULL,
-    foods_id integer,
-    gombs_id integer
+    food_id integer,
+    gsbomb_id integer
 );
 
 
 --
--- Name: foods_gombs_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+-- Name: foods_gsbombs_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
-ALTER TABLE public.foods_gombs ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME public.foods_gombs_id_seq
+ALTER TABLE public.foods_gsbombs ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.foods_gsbombs_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -130,19 +130,18 @@ ALTER TABLE public.users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- Data for Name: gombs; Type: TABLE DATA; Schema: public; Owner: -
+-- Data for Name: gsbombs; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.gombs (id, gombs_title, created_at, updated_at) FROM stdin;
+COPY public.gsbombs (id, gsbomb, created_at, updated_at) FROM stdin;
 1	Greens	2023-05-20 00:00:00	2023-05-20 00:00:00
 2	Onions	2023-05-20 00:00:00	2023-05-20 00:00:00
 3	Mushrooms	2023-05-20 00:00:00	2023-05-20 00:00:00
 4	Beans	2023-05-20 00:00:00	2023-05-20 00:00:00
 5	Berries	2023-05-20 00:00:00	2023-05-20 00:00:00
 6	Seeds	2023-05-20 00:00:00	2023-05-20 00:00:00
-7   Fruits	2023-05-20 00:00:00	2023-05-20 00:00:00
-8	Grains	2023-05-20 00:00:00	2023-05-20 00:00:00
-9	AntiNutri	2023-05-20 00:00:00	2023-05-20 00:00:00
+7	Fruits	2023-05-20 00:00:00	2023-05-20 00:00:00
+8	Antinuri	2023-05-20 00:00:00	2023-05-20 00:00:00
 \.
 
 
@@ -150,19 +149,19 @@ COPY public.gombs (id, gombs_title, created_at, updated_at) FROM stdin;
 -- Data for Name: foods; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.foods (id, fdb_id, food_title, food_description, food_image, created_at, updated_at) FROM stdin;
-1	72119190    Kale_raw    Kale_raw    /homepic.jpg    2023-05-20 00:00:00	2023-05-20 00:00:00
-2   72119211    Kale_fresh_raw  Kale freshraw   /homepic.jpg    2023-05-20 00:00:00	2023-05-20 00:00:00
-3   75103000    Cabbage_green_raw   Cabbage, green _raw /homepic.jpg    2023-05-20 00:00:00	2023-05-20 00:00:00
-4   75115000    Mushrooms   Mushrooms raw_  /homepic.jpg    2023-05-20 00:00:00	2023-05-20 00:00:00
+COPY public.foods (id, data_bank_id, food_name, food_description, created_at, updated_at) FROM stdin;
+1	72119190	Kale_raw	Kale_raw	2023-05-20 00:00:00	2023-05-20 00:00:00
+2	72119211	Kale_fresh_raw	Kale fresh,raw	2023-05-20 00:00:00	2023-05-20 00:00:00
+3	75103000	Cabbage_green_raw	Cabbage, green raw	2023-05-20 00:00:00	2023-05-20 00:00:00
+4	75115000	Mushrooms	Mushrooms raw_	2023-05-20 00:00:00	2023-05-20 00:00:00
 \.
 
 
 --
--- Data for Name: foods_gombs; Type: TABLE DATA; Schema: public; Owner: -
+-- Data for Name: foods_gsbombs; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.foods_gombs (id, foods_id, gombs_id) FROM stdin;
+COPY public.foods_gsbombs (id, food_id, gsbomb_id) FROM stdin;
 1	1	1
 2	2	1
 3	3	1
@@ -180,17 +179,17 @@ COPY public.users (id, first_name, last_name, email, password, created_at, updat
 
 
 --
--- Name: gombs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+-- Name: gsbombs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.gombs_id_seq', 9, true);
+SELECT pg_catalog.setval('public.gsbombs_id_seq', 8, true);
 
 
 --
--- Name: foods_gombs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+-- Name: foods_gsbombs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.foods_gombs_id_seq', 4, true);
+SELECT pg_catalog.setval('public.foods_gsbombs_id_seq', 4, true);
 
 
 --
@@ -208,19 +207,19 @@ SELECT pg_catalog.setval('public.users_id_seq', 1, true);
 
 
 --
--- Name: gombs gombs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: gsbombs gsbombs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.gombs
-    ADD CONSTRAINT gombs_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.gsbombs
+    ADD CONSTRAINT gsbombs_pkey PRIMARY KEY (id);
 
 
 --
--- Name: foods_gombs foods_gombs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: foods_gsbombs foods_gsbombs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.foods_gombs
-    ADD CONSTRAINT foods_gombs_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.foods_gsbombs
+    ADD CONSTRAINT foods_gsbombs_pkey PRIMARY KEY (id);
 
 
 --
@@ -240,19 +239,19 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: foods_gombs foods_gombs_gomb_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: foods_gsbombs foods_gsbombs_gsbomb_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.foods_gombs
-    ADD CONSTRAINT foods_gombs_gomb_id_fkey FOREIGN KEY (gombs_id) REFERENCES public.gombs(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY public.foods_gsbombs
+    ADD CONSTRAINT foods_gsbombs_gsbomb_id_fkey FOREIGN KEY (gsbomb_id) REFERENCES public.gsbombs(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
--- Name: foods_gombs foods_gombs_food_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: foods_gsbombs foods_gsbombs_food_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.foods_gombs
-    ADD CONSTRAINT foods_gombs_food_id_fkey FOREIGN KEY (foods_id) REFERENCES public.foods(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY public.foods_gsbombs
+    ADD CONSTRAINT foods_gsbombs_food_id_fkey FOREIGN KEY (food_id) REFERENCES public.foods(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
