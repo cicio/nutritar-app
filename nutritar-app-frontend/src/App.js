21e0e2bd574cd5e205a46d2988fa89ec8e132ce5
mwilsoncruz <8440577+cicio@users.noreply.github.com>
2023-05-24 23:22:51 -0400

    increased refreshToken interval

--- nutritar-app-frontend/src/App.js
+++ nutritar-app-frontend/src/App.js
@@ -50,7 +50,7 @@ function App () {
         .catch(error => {
           console.log("user is not logged in");
         })
-      }, 1000);
+      }, 600000);
       setTickInterval(i);
       console.log("setting ticking interval to: ", i);
     
